b6a6fd42153020eae900ca15cd107c4b2365eee3
Maengdok <pionaxel@gmail.com>
2023-04-30 17:07:20 +0200

    feat: create model, types, controller and routes to get exhibitors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { exhibitorRoutes } from './exhibitor.route';
 import { ticketRoutes } from './ticket.routes';
 import { userRoutes } from './user.routes';
+import { exhibitorRoutes } from './exhibitor.routes';
 
 export default {
   userRoutes,
