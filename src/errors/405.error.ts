d83754750d03a766af6dcee109a10a8ffed5afa3
Maengdok <pionaxel@gmail.com>
2023-05-01 13:43:49 +0200

    feat: add test for exhibitors and refactor error handling

--- src/errors/405.error.ts
+++ src/errors/405.error.ts
@@ -1,15 +1,13 @@
 import { HttpStatusCodes, HttpStatusCodesDescriptions } from "../environments/httpStatusCodes.environment";
 import { ApiError } from "./base.error";
 
-class Error405 extends ApiError {
+export class Error405 extends ApiError {
   constructor (
     name?: string,
-    statusCode: number = HttpStatusCodes.METHOD_NOT_ALLOWED,
-    description: string = HttpStatusCodesDescriptions.METHOD_NOT_ALLOWED,
+    statusCode: HttpStatusCodes = HttpStatusCodes.METHOD_NOT_ALLOWED,
+    description: HttpStatusCodesDescriptions = HttpStatusCodesDescriptions.METHOD_NOT_ALLOWED,
     isOperational?: boolean,
   ) {
     super({httpStatusCode: statusCode, description: description})
   }
 };
-
-module.exports = Error405;
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Router, Request, Response } from 'express';
 import { errorHandler } from './middlewares/errorHandler.middleware';
+import { Error404 } from '../src/errors/404.error';
 import routes from './routes';
 
 const router = Router();
 
 router.use('/users', routes.userRoutes);
 router.use('/exhibitors', routes.exhibitorRoutes);
+router.use((_request: Request, _response: Response, _next: NextFunction) => {
+  throw new Error404();
+});
 router.use((error: Error, _request: Request, response: Response, _next: NextFunction) => {
   errorHandler.handleError(error, response);
 });
