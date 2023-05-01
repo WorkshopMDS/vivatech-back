f464a9f153994a23a640ee4213375225137f1a59
Maengdok <pionaxel@gmail.com>
2023-05-01 18:36:18 +0200

    refactor: set error handling and api response on a single middleware

--- src/routes/exhibitor.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-import { getExhibitors } from '../controllers/exhibitor.controller';
-
-export const exhibitorRoutes: Router = Router();
-
-exhibitorRoutes.get('/', getExhibitors);
