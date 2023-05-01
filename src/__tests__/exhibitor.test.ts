c6c2436df0c690761c6e0d1df482c3e4e3974ac2
Maengdok <pionaxel@gmail.com>
2023-05-01 14:26:01 +0200

    feat: allow to fetch an exhibitor from its id

--- src/__tests__/exhibitor.test.ts
+++ src/__tests__/exhibitor.test.ts
@@ -1,9 +1,6 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
-
 import app from '../app';
-import { json } from 'stream/consumers';
-import { stringify } from 'querystring';
 
 const URI = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_ADDRESS}/${process.env.MONGO_ATLAS_DATABASE}?retryWrites=true&w=majority`;
 
--- src/controllers/exhibitor.controller.ts
+++ src/controllers/exhibitor.controller.ts
@@ -2,13 +2,33 @@ import type { Response, Request } from 'express';
 
 import Exhibitor from '../models/exhibitor.model';
 import type { IExhibitor } from '../types/exhibitor';
+import { Error404 } from '../errors/404.error';
+import { Error500 } from '../errors/500.error';
+import { HttpStatusCodes } from '../environments/httpStatusCodes.environment';
 
 export const getExhibitors = async (_req: Request, res: Response): Promise<void> => {
   try {
     const exhibitors: IExhibitor[] = await Exhibitor.find();
-    res.status(200).json({ exhibitors });
-  } catch (error) {
-    throw error;
+    res.status(200).json({exhibitors});
+  } catch (e) {
+    const error: Error500 = new Error500();
+    res.status(HttpStatusCodes.INTERNAL_SERVER).json({error});
+  }
+};
+
+export const getExhibitor = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const exhibitor: IExhibitor | null = await Exhibitor.findById(req.params.id);
+  
+    if (!exhibitor) {
+      const error: Error404 = new Error404();
+      res.status(HttpStatusCodes.NOT_FOUND).json({error});
+    }
+
+    res.status(200).json({exhibitor})
+  } catch (e) {
+    const error: Error500 = new Error500();
+    res.status(HttpStatusCodes.INTERNAL_SERVER).json({error});
   }
 };
 
