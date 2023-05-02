080c5addff2d2894f699e9f890215bbfc71c66ff
Maengdok <pionaxel@gmail.com>
2023-05-02 15:32:45 +0200

    feat: allow to update an exhibitor. Add tests for exhibitor update

--- src/tests/__tests__/exhibitor.test.ts
+++ src/tests/__tests__/exhibitor.test.ts
@@ -5,8 +5,10 @@ import { exhibitor } from '../mockups/exhibitor.mock';
 
 import _ from 'lodash';
 const URI = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_ADDRESS}/${process.env.MONGO_ATLAS_DATABASE}?retryWrites=true&w=majority`;
+let id: string | undefined = '';
 
 describe('GET /exhibitors', () => {
+
   beforeEach(async () => {
     await mongoose.connect(URI);
   });
@@ -54,12 +56,13 @@ describe('GET /exhibitors', () => {
   });
 
   describe('POST /exhibitor/add', () => {
-    it('should create a new exhibitor and return the exhibitor\s data', async() => {
+    it('should create a new exhibitor and return the exhibitor\'s data', async() => {
       const response = await request(app).post('/exhibitor/add').send(exhibitor.add);
       const expected = JSON.stringify(exhibitor.addResponse);
 
       expect(response.status).toBe(201);
       expect(typeof(response.body)).toBe("object");
+      id = response.body.data._id;
       _.unset(response.body, 'data._id');
       expect(JSON.stringify(_.omit(response.body, ['timestamp']))).toBe(expected);
       expect(typeof(response.body.data)).toBe("object");
@@ -71,7 +74,41 @@ describe('GET /exhibitors', () => {
 
       expect(JSON.stringify(_.omit(response.body, 'timestamp'))).toBe(expected);
     });
-  })
+  });
+
+  describe('PATCH /exhibitor/update', () => {
+    it('should update an exhibitor and return the exhibitor\'s data', async() => {
+      const response = await request(app).patch(`/exhibitor/update/${id}`).send(exhibitor.update);
+      const expected = JSON.stringify(exhibitor.updateResponse);
+
+      expect(response.status).toBe(200);
+      expect(typeof(response.body)).toBe("object");
+      _.unset(response.body, 'data._id');
+      expect(JSON.stringify(_.omit(response.body, ['timestamp']))).toBe(expected);
+      expect(typeof(response.body.data)).toBe("object");
+    });
+
+    it('should return an object of the error 400 if there is a missing value in body request.', async() => {
+      const response = await request(app).patch(`/exhibitor/update/${id}`).send(exhibitor.invalidUpdate);
+      const expected = JSON.stringify(exhibitor.error400);
+
+      expect(JSON.stringify(_.omit(response.body, 'timestamp'))).toBe(expected);
+    });
+
+    it('should return an object of the error 404 if exhibitor has not been found.', async() => {
+      const response = await request(app).patch(`/exhibitor/update/123456789098123456789098`).send(exhibitor.update);
+      const expected = JSON.stringify(exhibitor.error404);
+
+      expect(JSON.stringify(_.omit(response.body, 'timestamp'))).toBe(expected);
+    });
+
+    it('should return an object of the error 500 if the id is not correct.', async() => {
+      const response = await request(app).patch(`/exhibitor/update/12345678909812345678909`).send(exhibitor.update);
+      const expected = JSON.stringify(exhibitor.error500);
+
+      expect(JSON.stringify(_.omit(response.body, 'timestamp'))).toBe(expected);
+    });
+  });
 });
 
 
