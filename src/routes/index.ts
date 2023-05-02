75d4edf755d15a8c567a6a5cc0c56e5ed40cba91
Maengdok <pionaxel@gmail.com>
2023-05-02 17:02:23 +0200

    fix: remove unused import

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,5 @@
 import { exhibitorRoutes } from './exhibitor.route';
-import { ticketRoutes } from './ticket.routes';
 import { userRoutes } from './user.routes';
-import { exhibitorRoutes } from './exhibitor.routes';
 
 export default {
   userRoutes,
