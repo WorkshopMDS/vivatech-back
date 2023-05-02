46f4cc9be039b16ec60688b6619d0e715c2a15d4
Rémi Rubis <remi.rubis@yahoo.com>
2023-05-02 12:01:34 +0200

    feat(user): add routes

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,53 @@
+import type { NextFunction, Response } from 'express';
+import type { Secret } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+
+import { generateAccessToken } from '../controllers/user.controllers';
+import UserModel from '../models/user.model';
+import type { IRequest } from '../types/global.type';
+import type { IUserData } from '../types/user.type';
+import { errorFormatter, ErrorMessages } from '../utils/errors';
+import { generateComplexPassword } from '../utils/functions';
+
+export const createUserIfNotExist = async (req: IRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  // Use user account if is connected
+  if (token != null) return next();
+
+  try {
+    const newUser = new UserModel({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: generateComplexPassword(),
+    });
+    await newUser.save();
+
+    // Set an access token to use API
+    req.headers.authorization = `Bearer ${generateAccessToken({
+      id: newUser.id,
+      email: newUser.email,
+      role: newUser.role,
+    })}`;
+
+    return next();
+  } catch (error) {
+    return errorFormatter(res, 401, ErrorMessages.SERVER_ERROR, error);
+  }
+};
+
+export const isAuthenticated = (req: IRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return errorFormatter(res, 401, 'Unauthorized');
+  try {
+    const user: IUserData = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as Secret) as IUserData;
+    req.user = user;
+    return next();
+  } catch (error) {
+    return errorFormatter(res, 401, ErrorMessages.NOT_AUTHORIZED, error);
+  }
+};
