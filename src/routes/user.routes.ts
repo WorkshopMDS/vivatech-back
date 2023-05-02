82b424516e9525dd3f3995f3d4423d933ba19f5a
Rémi Rubis <remi.rubis@yahoo.com>
2023-05-02 13:49:50 +0200

    fix(user): add missing route who regenerate access token using refresh token

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { deleteUser, getUser, getUsers, login, register } from '../controllers/user.controllers';
+import { deleteUser, getUser, getUsers, login, refreshAccessToken, register } from '../controllers/user.controllers';
 import { isAuthenticated } from '../middlewares/auth.middleware';
 import { checkOwnership, isInGroup } from '../middlewares/permission.middleware';
 import { Roles } from '../utils/roles';
@@ -9,6 +9,7 @@ export const userRoutes: Router = Router();
 
 userRoutes.post('/register', register);
 userRoutes.post('/login', login);
+userRoutes.post('/refreshToken', refreshAccessToken);
 userRoutes.get('/users', isAuthenticated, isInGroup([Roles.ADMIN]), getUsers);
 userRoutes.get('/user', isAuthenticated, getUser);
 userRoutes.get('/user/:userId', isAuthenticated, checkOwnership('Users', 'userId', '_id'), getUser);
