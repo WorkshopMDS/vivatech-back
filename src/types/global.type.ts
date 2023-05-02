8711f7d2d19376d4ae9db00f0896484b5fb13ecd
Rémi Rubis <remi.rubis@yahoo.com>
2023-05-03 00:11:49 +0200

    feat(speaker): add new role speaker

--- src/types/global.type.ts
+++ src/types/global.type.ts
@@ -1,7 +1,16 @@
 import type { Request } from 'express';
+import { Document } from 'mongoose';
 
 import type { IUser } from './user.type';
 
 export interface IRequest extends Request {
   user?: Pick<IUser, 'id' | 'email' | 'role'>;
+}
+
+export interface ILinks {
+  type: {
+    type: string;
+    enum: ['facebook', 'twitter', 'instagram', 'linkedin', 'github', 'website', 'other'];
+  };
+  url: String;
 }
