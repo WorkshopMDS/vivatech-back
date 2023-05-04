cbdaba34354c91172e431b7452a83e56fed37697
Rémi Rubis <remi.rubis@yahoo.com>
2023-05-04 23:44:08 +0200

    refactor(speaker.test): mock returns values and add new test cases

--- src/__tests__/speaker.test.ts
+++ src/__tests__/speaker.test.ts
@@ -1,8 +1,11 @@
 import { afterAll } from '@jest/globals';
+import _ from 'lodash';
 import request from 'supertest';
 
-import { expectError, generateUser } from './configs/functions';
+import { UNKNOWN_ID } from './configs/constants';
+import { expectError, generateUser, omitTimestamp } from './configs/functions';
 import * as db from './configs/setup';
+import { speakers } from './mockups/speaker.mock';
 import app from '../app';
 import type { IUser, IUserDocument } from '../types/user.type';
 import { Roles } from '../utils/roles';
@@ -12,6 +15,8 @@ describe('test_speaker_feature_routes', () => {
   let userAccessToken: string;
   let adminUser: IUserDocument;
   let adminAccessToken: string;
+
+  let speaker: IUser;
 
   beforeAll(async () => {
     await db.connect();
@@ -25,9 +30,21 @@ describe('test_speaker_feature_routes', () => {
 
   describe('POST /speaker', () => {
     it("should create a user if is not exist and I'm admin", async () => {
-      const response = await request(app).post('/speaker').set('Authorization', `Bearer ${adminAccessToken}`).send({
-        email: 'new+speaker@example.com',
-      });
+      const response = await request(app)
+        .post('/speaker')
+        .set('Authorization', `Bearer ${adminAccessToken}`)
+        .send({
+          email: 'new+speaker@example.com',
+          firstname: 'Speaker',
+          lastname: 'Example',
+          company: {
+            name: 'Speaker Company',
+            title: 'CE0',
+          },
+          biography: "I'm a speaker",
+          picture: 'https://example.com/speaker.jpg',
+        });
+      speaker = response.body.data;
 
       expect(response.status).toBe(201);
       expect(response.body.data.role).toBe(Roles.SPEAKER);
@@ -67,9 +84,34 @@ describe('test_speaker_feature_routes', () => {
   describe('GET /speakers', () => {
     it('should return all users who have role speaker', async () => {
       const response = await request(app).get('/speakers');
+      const expected = JSON.stringify(speakers.speakerReturn);
 
       expect(response.status).toBe(200);
-      expect(response.body.data).toHaveLength(2);
+
+      _.unset(response.body.data[1], 'id');
+      expect(JSON.stringify(omitTimestamp(response.body.data[1]))).toBe(expected);
+    });
+  });
+
+  describe('GET /speaker', () => {
+    it('should return speaker with specific id', async () => {
+      const response = await request(app).get(`/speaker/${speaker.id}`);
+      const expected = JSON.stringify(speakers.getSpeaker);
+
+      expect(response.status).toBe(200);
+
+      _.unset(response.body, 'data.id');
+      expect(JSON.stringify(omitTimestamp(response.body))).toBe(expected);
+    });
+
+    it('should return 404 if speaker id not exist', async () => {
+      const response = await request(app).get(`/speaker/${UNKNOWN_ID}`);
+      expectError(response, 404);
+    });
+
+    it('should return 500 if id is malformed', async () => {
+      const response = await request(app).get(`/speaker/unknown`);
+      expectError(response, 500);
     });
   });
 });
