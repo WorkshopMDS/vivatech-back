0ed0356424782806073edb211df023ed82b6cfeb
Rémi Rubis <remi.rubis@yahoo.com>
2023-05-04 02:40:20 +0200

    refactor(exhibitor): edit routes to add middlewares

--- src/routes/exhibitor.route.ts
+++ src/routes/exhibitor.route.ts
@@ -7,11 +7,14 @@ import {
   updateExhibitor,
   deleteExhibitor,
 } from '../controllers/exhibitor.controller';
+import { isAuthenticated } from '../middlewares/auth.middleware';
+import { isInGroup } from '../middlewares/permission.middleware';
+import { Roles } from '../utils/roles';
 
 export const exhibitorRoutes: Router = Router();
 
 exhibitorRoutes.get('/exhibitors', getExhibitors);
 exhibitorRoutes.get('/exhibitor/:id', getExhibitor);
-exhibitorRoutes.post('/exhibitor/add', addExhibitor);
-exhibitorRoutes.patch('/exhibitor/update/:id', updateExhibitor);
-exhibitorRoutes.delete('/exhibitor/delete/:id', deleteExhibitor);
+exhibitorRoutes.post('/exhibitor', isAuthenticated, isInGroup([Roles.ADMIN]), addExhibitor);
+exhibitorRoutes.patch('/exhibitor/:id', isAuthenticated, isInGroup([Roles.ADMIN]), updateExhibitor);
+exhibitorRoutes.delete('/exhibitor/:id', isAuthenticated, isInGroup([Roles.ADMIN]), deleteExhibitor);
